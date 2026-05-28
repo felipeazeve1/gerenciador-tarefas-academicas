dd1660396a13ba739de1f869870c1da907728947
Felipe <felipeazeve1@outlook.com>
2026-05-28 11:03:18 -0300

    Fix: Removendo tarefas.json e pycache do rastreamento do Git

--- gui.py
+++ gui.py
@@ -24,7 +24,6 @@ def criar_janela():
     ent_prazo = tk.Entry(janela, width=40)
     ent_prazo.pack(pady=5)
     
-    #FUNÇÃO: ATUALIZAR LISTA VISUAL 
     def atualizar_lista_visual():
         lista_visual.delete(0, tk.END)
         for tarefa in dados_tarefas:
@@ -32,7 +31,6 @@ def criar_janela():
             texto_linha = f"{status} {tarefa['titulo']} ({tarefa['materia']}) - Prazo: {tarefa['prazo']}"
             lista_visual.insert(tk.END, texto_linha)
 
-    #LÓGICA DO BOTÃO ADICIONAR 
     def acao_adicionar():
         t = ent_titulo.get()
         m = ent_materia.get()
@@ -58,11 +56,9 @@ def criar_janela():
         ent_materia.delete(0, tk.END)
         ent_prazo.delete(0, tk.END)
 
-    #BOTÃO ADICIONAR 
     btn_adicionar = tk.Button(janela, text="Adicionar Tarefa", bg="#4CAF50", fg="white", font=("Arial", 10, "bold"), command=acao_adicionar)
     btn_adicionar.pack(pady=15)
 
-    #LISTA VISUAL NO APP 
     lbl_suas_tarefas = tk.Label(janela, text="Suas Tarefas (Clique para selecionar):", font=("Arial", 10, "bold"))
     lbl_suas_tarefas.pack(pady=(10, 0))
 
@@ -72,7 +68,6 @@ def criar_janela():
     
     def acao_concluir():
         try:
-            # lista_visual.curselection() devolve o índice da linha que o usuário clicou
             indice_selecionado = lista_visual.curselection()[0]
             
             dados_tarefas[indice_selecionado]["concluida"] = True
@@ -81,14 +76,12 @@ def criar_janela():
             atualizar_lista_visual()
 
         except IndexError:
-            # Se o usuário clicar no botão sem ter selecionado nada na lista, dá IndexError
             messagebox.showwarning("⚠️ Erro", "Por favor, selecione uma tarefa da lista primeiro!")
 
     def acao_excluir():
         try:
             indice_selecionado = lista_visual.curselection()[0]
             
-            #Remoção da lista usando o pop()
             dados_tarefas.pop(indice_selecionado)
             
             salvar_tarefas(dados_tarefas)
@@ -98,12 +91,11 @@ def criar_janela():
         except IndexError:
             messagebox.showwarning("⚠️ Erro", "Por favor, selecione uma tarefa da lista primeiro!")
 
-    # Criação do 'Frame' (uma caixinha invisível) para colocar os dois botões lado a lado
     frame_botoes = tk.Frame(janela)
     frame_botoes.pack(pady=10)
 
     btn_concluir = tk.Button(frame_botoes, text="Concluir Tarefa", bg="#2196F3", fg="white", font=("Arial", 10, "bold"), command=acao_concluir)
-    btn_concluir.pack(side=tk.LEFT, padx=10) # side=tk.LEFT alinha à esquerda dentro do frame
+    btn_concluir.pack(side=tk.LEFT, padx=10) 
 
     btn_excluir = tk.Button(frame_botoes, text="Excluir Tarefa", bg="#F44336", fg="white", font=("Arial", 10, "bold"), command=acao_excluir)
     btn_excluir.pack(side=tk.LEFT, padx=10)
